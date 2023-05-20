9f75d89d6f3ab0906cb5e6e063b9ecfd743425ee
Prabhakar Reddy <Prabhakareddy006@gmail.com>
2023-05-20 23:04:02 +0530

    49298-Second Commit

--- SpringBootTestApp/src/SpringBootTestApp/Messages.java
+++ SpringBootTestApp/src/SpringBootTestApp/Messages.java
@@ -1,11 +1,10 @@
 package SpringBootTestApp;
 
-public class Runner {
+public class Messages {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hellow World");
-System.out.println("Hello Modified");
+
 	}
 
 }
